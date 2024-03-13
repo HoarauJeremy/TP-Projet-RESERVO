bd55f606cf7c4c35ee684cf08ce5d7fc5ec30ca0
jeremy <jeremyhoarau44@gmail.com>
2024-03-13 17:04:31 +0400

    finalisation du formulaire

--- pageUnique/index.php
+++ pageUnique/index.php
@@ -47,19 +47,27 @@
                     <tbody>
                         <tr>
                             <td>Préau</td>
+                            <td>50</td>
+                        </tr>
+                        <tr>
+                            <td>Terrain</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Salle 1</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Salle 2</td>
+                            <td>100</td>
+                        </tr>
+                        <tr>
+                            <td>Centre culturel 1</td>
                             <td>150</td>
                         </tr>
                         <tr>
-                            <td>Lorem.</td>
-                            <td>Lorem.</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem.</td>
-                            <td>Lorem.</td>
-                        </tr>
-                        <tr>
-                            <td>Lorem.</td>
-                            <td>Lorem.</td>
+                            <td>Centre culturel 2</td>
+                            <td>150</td>
                         </tr>
                     </tbody>
                 </table>
@@ -76,23 +84,27 @@
                     <tbody>
                         <tr>
                             <td>Table</td>
-                            <td></td>
+                            <td>8</td>
                         </tr>
                         <tr>
-                            <td>Table</td>
-                            <td></td>
+                            <td>Chaise</td>
+                            <td>2 <!--/Jour--></td>
                         </tr>
                         <tr>
-                            <td>Table</td>
-                            <td></td>
+                            <td>Matériel de sonorisation</td>
+                            <td>75</td>
                         </tr>
                         <tr>
-                            <td>Table</td>
-                            <td></td>
+                            <td>Chapiteau 3x3m</td>
+                            <td>100</td>
                         </tr>
                         <tr>
-                            <td>Table</td>
-                            <td></td>
+                            <td>Chapiteau 3x4m</td>
+                            <td>130</td>
+                        </tr>
+                        <tr>
+                            <td>Chapiteau 3x6m</td>
+                            <td>180</td>
                         </tr>
                     </tbody>
                 </table>
@@ -159,56 +171,62 @@
                 <div class="salle">
                     <h4>Salle</h4>
                     <div class="">
-                        <label for="salle">Préau</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="preau">Préau</label>
+                        <input type="checkbox" name="preau" id="preau" value="p">
                     </div>
                     <div class="">
-                        <label for="salle">Terrain</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="terrain">Terrain</label>
+                        <input type="checkbox" name="terrain" id="terrain" value="t">
                     </div>
                     <div class="">
-                        <label for="salle">Salle 1</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="salle1">Salle 1</label>
+                        <input type="checkbox" name="salle1" id="salle1" value="s1">
                     </div>
                     <div class="">
-                        <label for="salle">Salle 2</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="salle2">Salle 2</label>
+                        <input type="checkbox" name="salle2" id="salle2" value="s2">
                     </div>
                     <div class="">
-                        <label for="salle">Centre culturel 1</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="centreCulturel1">Centre culturel 1</label>
+                        <input type="checkbox" name="centreCulturel1" id="centreCulturel1" value="cc1">
                     </div>
                     <div class="">
-                        <label for="salle">Centre culturel 2</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="centreCulturel2">Centre culturel 2</label>
+                        <input type="checkbox" name="centreCulturel2" id="centreCulturel2" value="cc2">
                     </div>
                 </div>
 
-<!--                 <div class="equipement">
+                <div class="equipement">
                     <h4>Equipement</h4>
                     <div class="">
-                        <label for="salle">Préau</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="table">Table</label>
+                        <input type="checkbox" name="table" id="table" value="table">
+                        <input type="number" name="nbTable" id="nbTable">
                     </div>
                     <div class="">
-                        <label for="salle">Terrain</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="chaise">Chaise</label>
+                        <input type="checkbox" name="chaise" id="chaise" value="chaise">
+                        <input type="number" name="nbChaise" id="nbChaise">
                     </div>
                     <div class="">
-                        <label for="salle">Salle 1</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="sono">Matériel de sonorisation</label>
+                        <input type="checkbox" name="sono" id="sono" value="sono">
+                        <input type="number" name="nbSono" id="nbSono">
                     </div>
                     <div class="">
-                        <label for="salle">Salle 2</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="chapiteau3-3">Chapiteau 3x3m</label>
+                        <input type="checkbox" name="chapiteau3-3" id="chapiteau3-3" value="c3-3">
+                        <input type="number" name="nbChapiteau3-3" id="nbChapiteau3-3">
                     </div>
                     <div class="">
-                        <label for="salle">Centre culturel 1</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="chapiteau3-4">Chapiteau 3x4m</label>
+                        <input type="checkbox" name="chapiteau3-4" id="chapiteau3-4" value="c3-4">
+                        <input type="number" name="nbChapiteau3-4" id="nbChapiteau3-4">
                     </div>
                     <div class="">
-                        <label for="salle">Centre culturel 2</label>
-                        <input type="checkbox" name="salle" id="salle">
+                        <label for="chapiteau3-6">Chapiteau 3x6m</label>
+                        <input type="checkbox" name="chapiteau3-6" id="chapiteau3-6" value="c3-6">
+                        <input type="number" name="nbChapiteau3-6" id="nbChapiteau3-6">
                     </div>
                 </div>
 
@@ -216,20 +234,15 @@
                     <h4>Service</h4>
                     <div class="">
                         <label for="service-MeP">Mise en place</label>
-                        <input type="radio" name="service" id="service-MeP">
+                        <input type="checkbox" name="service-MeP" id="service-MeP" value="MeP">
                     </div>
                     <div class="">
                         <label for="service-NeR">Nettoyage et Rangement</label>
-                        <input type="radio" name="service" id="service-NeR">
+                        <input type="checkbox" name="service-NeR" id="service-NeR" value="NeR">
                     </div>
-                    <div class="">
-                        <label for="service-AS">Aucun service</label>
-                        <input type="radio" name="service" id="service-AS" checked>
-                    </div>
-                </div> -->
+                </div>
 
                 <input type="submit" name="submit" id="submit" value="Réserver">
-                <!-- <input type="submit" id="submit">Réserver</input> -->
             </form>
         </div>
     </div>
@@ -246,6 +259,32 @@
         $timeStart = $_POST['time-start'];
         $timeEnd = $_POST['time-end'];
 
+        /* Salle */
+        $p = (isset($_POST['preau']) && $_POST['preau'] == "p") ? 50 : 0;
+        $t = (isset($_POST['terrain']) && $_POST['terrain'] == "t") ? 100 : 0;
+        $s1 = (isset($_POST['salle1']) && $_POST['salle1'] == "s1") ? 100 : 0;
+        $s2 = (isset($_POST['salle2']) && $_POST['salle2'] == "s2") ? 100 : 0;
+        $cc1 = (isset($_POST['centreCulturel1']) && $_POST['centreCulturel1'] == "cc1") ? 150 : 0;
+        $cc2 = (isset($_POST['centreCulturel2']) && $_POST['centreCulturel2'] == "cc2") ? 150 : 0;
+
+        $prixSalle = $p + $t + $s1 + $s2 + $cc1 + $cc2;
+
+        /* Equipement */
+        $Table = (isset($_POST['table']) && $_POST['table'] == "table" && isset($_POST['nbTable'])) ? (8 * $_POST['nbTable']) : 0;
+        $Chaise = (isset($_POST['chaise']) && $_POST['chaise'] == "chaise" && isset($_POST['nbChaise'])) ? (2 * $_POST['nbChaise']) : 0;
+        $Sono = (isset($_POST['sono']) && $_POST['sono'] == "sono" && isset($_POST['nbSono'])) ? (75 * $_POST['nbSono']) : 0;
+        $Chapiteau_3_3 = (isset($_POST['chapiteau3-3']) && $_POST['chapiteau3-3'] == "c3-3" && isset($_POST['nbChapiteau3-3'])) ? (100 * $_POST['nbChapiteau3-3']) : 0;
+        $Chapiteau_3_4 = (isset($_POST['chapiteau3-4']) && $_POST['chapiteau3-4'] == "c3-4" && isset($_POST['nbChapiteau3-4'])) ? (130 * $_POST['nbChapiteau3-4']) : 0;
+        $Chapiteau_3_6 = (isset($_POST['chapiteau3-6']) && $_POST['chapiteau3-6'] == "c3-6" && isset($_POST['nbChapiteau3-6'])) ? (180 * $_POST['nbChapiteau3-6']) : 0;
+
+        $prixEquipement = $Table + $Chaise + $Sono + $Chapiteau_3_3 + $Chapiteau_3_4 + $Chapiteau_3_6;
+
+        /* Service */
+        $service_MeP = (isset($_POST['service-MeP']) && $_POST['service-MeP'] == "MeP") ? 150 : 0;
+        $service_NeR = (isset($_POST['service-NeR']) && $_POST['service-NeR'] == "NeR") ? 250 : 0;
+
+        $prixService = $service_MeP + $service_NeR;
+
         echo "
             <ul>
                 <li>$Fname</li>
@@ -255,15 +294,19 @@
                 <li>$date</li>
                 <li>$timeStart</li>
                 <li>$timeEnd</li>
+                <li>Prix Salle: $prixSalle</li>
+                <li>Prix Equipement: $prixEquipement</li>
+                <li>Prix Service: $prixService</li>
+                augmentation de 5%/H
             </ul>
         ";
-
     }
 
 ?>
 
 <footer>
-    <h1>Lorem.</h1>
+    <h2>RESERVO</h2>
+    <p>Association AJS &copy;</p>
 </footer>
     
 </body>
